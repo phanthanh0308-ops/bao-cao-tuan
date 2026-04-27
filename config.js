af91d7225a612ebb92c4906497d131f9a454c6e0
Phan Thành <phanthanh0308@gmail.com>
2026-04-27 14:17:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var APP_CONFIG = {
   //  Lấy từ: Apps Script → Deploy → Manage deployments → URL
   //  Mỗi lần deploy lại phải cập nhật URL mới vào đây
   // ----------------------------------------------------------
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwAvk7BiYLMn1Cz1bTPDRYyXAisI3hYJldfERyjc_61lj2uJMqEc64XE4t9-VnliTXnNA/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzPKbBwOhx8ANXhwax67mWAJw7OOuknK19mKW1kKvlA-z0RH3Nq4UT8oRKJHSjhwD04pw/exec',
 
   // ----------------------------------------------------------
   //  GOOGLE SHEET IDs
